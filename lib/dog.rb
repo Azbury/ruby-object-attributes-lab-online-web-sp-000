dea3789ea37e7d9a0e54b2dc14a772fa2f83683d
Austin Asbury <acasbury25@aol.com>
2019-08-25 12:47:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -15,3 +15,21 @@ class Dog
     @breed
   end
 end
+
+class Person
+  def name= (person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+  def job= (job_name)
+    @job = job_name
+  end
+
+  def
+    @job
+  end
+end
